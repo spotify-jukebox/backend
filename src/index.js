4d758c243a345ab0aedbe444f79e3e307054b780
Henri Nurmi <henri.nurmi@kapsi.fi>
2017-03-09 14:39:56 +0200

    Word generator

--- src/index.js
+++ src/index.js
@@ -11,27 +11,58 @@ function send_notification(device_id, url) {
 
 }
 
+function generate_id(length) {
+	let words = ["alligator", "ant","bear","bee","bird","camel","cat","cheetah","chicken","chimpanzee","cow",
+				"crocodile","deer","dog","dolphin","duck","eagle","elephant","fish","fly","fox","frog","giraffe",
+				"goat","goldfish","hamster","hippopotamus","horse","kangaroo","kitten","lion","lobster","monkey",
+				"octopus","owl","panda","pig","puppy","rabbit","rat","scorpion","seal","shark","sheep","snail",
+				"snake","spider","squirrel","tiger","turtle","wolf","zebra"]
+
+	let res = ""
+	for(let i = 0; i < length; i++) {
+		let word = words[Math.floor(Math.random()*words.length)]
+		res += word	
+
+		if (i < length - 1) {
+			res += "-"
+		}
+	}
+
+	return res
+}
+
 app.post('/generate', function (req, res) {
 	let device_token = req.body.device_token
-	
-	let token_id = Math.ceil(Math.random()*10000)
-	clients[token_id] = token_id
 
-	res.json({token: token_id.toString()})
+	while(true) {
+		let id = generate_id(3)
+		
+		if (!clients[id]) {
+			clients[id] = {'device_token': device_token}
+			res.json({token: id})
+
+			break
+		}
+	}
 })
 
 app.post("/addTrack/:token", function (req, res) {
 	let token_id = req.params.token
 	
-		if (clients[token_id]) {
+	if (clients[token_id]) {
+		if (req.body.track_url) {	
 			send_notification(clients[token_id], req.body.track_url)
 			res.json({status: "ok"})
 		}
 		else {
-			res.json({error: "Invalid token"})
+			res.json({error: "Give url"})
 		}
+	}
+	else {
+		res.json({error: "Invalid token"})
+	}
 })
-app.listen(port, function () {
-	  console.log('Example app listening on port 3000!')
 
+app.listen(port, function () {
+	  console.log('Listening on port ' + port)
 })
