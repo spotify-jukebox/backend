07c98a7d4b5bd8f93054727b648e4a4038124ed1
Henri Nurmi <henri.nurmi@kapsi.fi>
2017-04-23 15:22:13 +0300

    Refactored

--- src/index.js
+++ src/index.js
@@ -7,52 +7,52 @@ let port = process.env.PORT || 3000
 app.use(bodyParser.json())
 
 let options = {
-  token: {
-    key: "key",
-    keyId: "",
-    teamId: ""
-  },
-  production: false
+	cert: "cert/cert.pem",
+	key: "cert/key.pem",
+	production: false
 }
-let apnProvider = undefined//new apn.Provider(options);
+let apnProvider = new apn.Provider(options);
 
 let clients = {}
 
-function send_notification(device_id, url) {
-	let note = new apn.Notification();
-
-	note.expiry = Math.floor(Date.now() / 1000) + 3600;
-	note.payload = {'track_url': url};
-	note.topic = "fi.vaaraj.jukeboxly";
-
-	apnProvider.send(note, deviceToken).then( (result) => {
-	} );
+let require_list = (req, res, next) => {
+	if (req.params.token && clients[req.params.token]) {
+		req.client = clients[req.params.token]
+		next()
+	}
+	else {
+		res.json({error: "Invalid token"})
+	}	
 }
 
-function generate_id(length) {
+let send_notification = (device_id, url) => {
+	let note = new apn.Notification()
+
+	note.expiry = Math.floor(Date.now() / 1000) + 3600
+	note.payload = {'track_url': url}
+	note.topic = "fi.vaaraj.jukeboxly"
+
+	apnProvider.send(note, deviceToken).then( (result) => {
+	} )
+}
+
+let generate_id = (length) => {
 	let words = ["alligator", "ant","bear","bee","bird","camel","cat","cheetah","chicken","chimpanzee","cow",
 				"crocodile","deer","dog","dolphin","duck","eagle","elephant","fish","fly","fox","frog","giraffe",
 				"goat","goldfish","hamster","hippopotamus","horse","kangaroo","kitten","lion","lobster","monkey",
 				"octopus","owl","panda","pig","puppy","rabbit","rat","scorpion","seal","shark","sheep","snail",
 				"snake","spider","squirrel","tiger","turtle","wolf","zebra"]
 
-	let res = ""
-	for(let i = 0; i < length; i++) {
-		let word = words[Math.floor(Math.random()*words.length)]
-		res += word	
-
-		if (i < length - 1) {
-			res += "-"
-		}
-	}
-
-	return res
+	return new Array(length)
+		.fill(undefined)
+		.map(_ => words[Math.floor(Math.random() * words.length)])
+		.join("-")
 }
 
-app.post('/generate', function (req, res) {
+app.post('/generate', (req, res) => {
 	let device_token = req.body.device_token
 
-	if (device_token) {
+	if (!device_token) {
 		res.json({error: "Give device token"})
 		return 
 	}
@@ -61,29 +61,31 @@ app.post('/generate', function (req, res) {
 		let id = generate_id(3)
 		
 		if (!clients[id]) {
-			clients[id] = {'device_token': device_token}
-			res.json({token: id})
+			clients[id] = {
+				device_token: device_token,
+				name: req.body.name ? req.body.name : "No name"
+			}
 
+			res.json({token: id})
 			break
 		}
 	}
 })
 
-app.post("/addTrack/:token", function (req, res) {
-	let token_id = req.params.token
-	
-	if (clients[token_id]) {
-		if (req.body.track_url) {	
-			//send_notification(clients[token_id], req.body.track_url)
-			res.json({status: "ok"})
-		}
-		else {
-			res.json({error: "Give url"})
-		}
+app.post("/list/:token/", require_list, (req, res) => {
+	if (req.body.track_url) {	
+		//send_notification(req.client.name, req.body.track_url)
+		res.json({status: "ok"})
 	}
 	else {
-		res.json({error: "Invalid token"})
+		res.json({error: "Give url"})
 	}
+})
+
+app.get("/list/:token", require_list, (req, res) => {
+	res.json({
+		name: req.client.name
+	})
 })
 
 app.get("/", function(req, res) {
